477111e759f7d501e3a17e530156945f5fd6499c
bufferoverf1ow <bufferoverf1ow@icloud.com>
2022-10-07 09:05:54 +0900

    Release. 1

---
+++ stat.py
@@ -0,0 +1,81 @@
+import argparse
+import csv
+import datetime
+import pandas
+
+# constants
+FILE = []
+SIZE = []
+INODE = []
+ACESS = []
+UID = []
+GID = []
+ATIME = []
+MTIME = []
+CTIME = []
+BTIME = []
+
+
+def getTimeStamp(timeList):
+    if len(timeList) == 3:
+        UTC_INFO = timeList[2]
+    
+    linuxTime = ' '.join(timeList[0:2])
+    return datetime.datetime.strptime(linuxTime[:-10], "%Y-%m-%d %H:%M:%S")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest='stat_file')
+    parser.add_argument('-o', dest='output_filename')
+    args = parser.parse_args()
+    
+    if args.stat_file is None:
+        print(f"stat 명령어 결과 파일에 대한 경로를 입력하세요!")
+        exit(0)
+    else:
+        STAT_FILE = args.stat_file
+        
+        if args.output_filename is None:
+            OUTPUT_FILE = "stat.csv"
+        else:
+            OUTPUT_FILE = args.output_filename
+    
+    f = open(STAT_FILE, 'r')
+    lines = [line.rstrip() for line in f.readlines()]
+    
+    if 'Birth' in lines[7]: # Birth 정보가 있는 파일
+        JMP_IDX = 8 # Birth: 가 존재하면 step 8
+    else: # Birth 정보가 없는 파일
+        JMP_IDX = 7 # Birth: 가 존재하지 않으면 step  7
+        
+    for i in range(0, len(lines), JMP_IDX):
+        FILE.append(lines[i].split()[1]) # FILE
+        SIZE.append(lines[i+1].split()[1]) # SIZE
+        INODE.append(lines[i+2].split()[3]) # INODE
+        ACESS.append(lines[i+3].split()[1][1:-1]) # ACESS
+        UID.append(lines[i+3].split()[5][:-1]) # UID
+        GID.append(lines[i+3].split()[9][:-1]) # GID
+        ATIME.append(getTimeStamp(lines[i+4].split()[1:]).strftime("%Y-%m-%d %H:%M:%S")) # ATIME
+        MTIME.append(getTimeStamp(lines[i+5].split()[1:]).strftime("%Y-%m-%d %H:%M:%S")) # MTIME
+        CTIME.append(getTimeStamp(lines[i+6].split()[1:]).strftime("%Y-%m-%d %H:%M:%S")) # CTIME
+        if JMP_IDX != 7:
+            BTIME.append(getTimeStamp(lines[i+7].split()[1:]).strftime("%Y-%m-%d %H:%M:%S")) # BTIME
+            
+    df = pandas.DataFrame(FILE, columns=['File'])
+    df['Size'] = SIZE
+    df['Inode'] = INODE
+    df['Acess Control'] = ACESS
+    df['Uid'] = UID
+    df['Gid'] = GID
+    df['Acess Time'] = ATIME
+    df['Modify Time'] = MTIME
+    df['Change Time'] = CTIME
+    if JMP_IDX != 7:
+        df['Birth Time'] = BTIME
+    
+    df.to_csv(OUTPUT_FILE)
+
+
+if __name__ == "__main__":
+    main()
